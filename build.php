3f5a74f80c42e33ae58bedc0ed00e215e5799df3
Mike <mike@feijs.nl>
2016-03-10 15:55:41 +0100

    make readme pretty

--- build.php
+++ build.php
@@ -1,3 +1,7 @@
 <?php 
 
-passthru("tree --noreport -o readme.md .");
+passthru("tree --noreport -o tree.txt .");
+passthru("echo \"\`\`\`\n\" > readme.md");
+passthru("cat  tree.txt >> readme.md");
+passthru("echo \"\n\`\`\`\" >> readme.md");
+passthru("rm tree.txt");
